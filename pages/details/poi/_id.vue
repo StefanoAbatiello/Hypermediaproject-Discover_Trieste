a983e5f3b5fd51d1615474f306678566ca9f2e8c
stefanoTaborelli <10608112@polimi.it>
2022-05-15 19:43:02 +0200

    itineraries fixes

--- pages/details/poi/_id.vue
+++ pages/details/poi/_id.vue
@@ -3,6 +3,12 @@
     <div class="bg-primary text-white text-center">
       <h1>{{ name }}</h1>
     </div>
+    <ul class="breadcrumb">
+      <li class="breadcrumb-item"><a @click="backToList()">allPoi</a></li>
+      <li class="breadcrumb-item"><a href="#">Summer 2017</a></li>
+      <li class="breadcrumb-item"><a href="#">Italy</a></li>
+      <li class="breadcrumb-item active">Rome</li>
+    </ul>
     <img :src="img" class="img-fluid mx-auto d-block" />
     <div class="container-fluid m-0 p-0">
       Info:<br />
@@ -27,6 +33,13 @@
         commodo consequat...
       </p>
     </div>
+    <ul class="pagination">
+        <li class="page-item"><a class="page-link" @click="next()">Previous</a></li>
+        <li class="page-item"><a class="page-link" @click="next(id)">Next</a></li>
+    </ul>
+    <script>
+      console.log({{id}});
+    </script>
   </div>
 </template>
 
@@ -38,16 +51,14 @@ export default {
   async asyncData({ route, $axios }) {
     const { id } = route.params
     const { data } = await $axios.get('/api/pois/' + id)
-    
     return {
+      id,
       name: data.name,
       img: data.img,
       description: data.description,
     }
   },
-  data(){
-
-  },
+  data() {},
   head() {
     return {
       title: this.name,
@@ -59,8 +70,12 @@ export default {
     // console.log(this.formatMyDate(date.toLocaleDateString()))
   },
   methods: {
+    next(id){
+      const temp= parseInt(id) + 1;
+      this.$router.push('/details/poi/'+temp);
+    },
     backToList() {
-      this.$router.push('/itineraries')
+      this.$router.push('/pois')
     },
     accessibilityPrint() {
       document.getElementById('test').innerHTML = 'accessibility'
