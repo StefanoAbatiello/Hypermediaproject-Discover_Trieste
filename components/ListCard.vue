c22e41f8a8e89a59a9aeb669d6f2275ccaa22633
StefanoAbatiello <80035914+StefanoAbatiello@users.noreply.github.com>
2022-05-18 16:32:07 +0200

    listcard design adjustment

--- components/ListCard.vue
+++ components/ListCard.vue
@@ -2,9 +2,9 @@
   <div class="rounded shadow  category-image" style="width: 23rem" :style="{ 'background-image': 'url(' + img + ')' }" >
      
     <div class="category-body">
-      <h4 class="category-title text-center">{{ name }}</h4>
+      <!-- <h4 class="category-title text-center">{{ name }}</h4> -->
       <nuxt-link :to="`/details/${category}/${id}`">
-        <div class="btn text-white btn-details"> See details...</div>
+        <div class="btn text-white btn-details"> {{ name }} </div>
       </nuxt-link>
     </div>
   </div>
@@ -43,25 +43,30 @@ export default {
 <style scoped>
 .category-image:hover {
   border: 3px solid rgb(195, 75, 75);
+  opacity: 1;
 }
 .btn-details {
-  background: rgba(225, 216, 216, 0.734);
+  background: rgb(195, 75, 75);
   text-shadow: 2px 2px 4px black;
-  position: relative;
-  left: 20px;
-  top: 160px;
+  opacity:1;
+  position: absolute;
+  left: 50%;
+  top: 85%;
+  transform: translate(-50%,-50%);
 }
-.category-title {
+/* .category-title {
   font-style: italic;
   text-align: left;
   color: white;
   text-shadow: 3px 3px 4px black;
-}
+} */
 .category-image {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   height: 250px;
+  opacity: 0.6;
+  position: relative;
 }
 </style>
 
