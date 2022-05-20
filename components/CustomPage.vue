a572e88cc17ed03e601a803eff5601ad4448b89d
alessandrobianco <alessandro2.bianco@mail.polimi.it>
2022-05-20 10:43:18 +0200

    buttons modified

--- components/CustomPage.vue
+++ components/CustomPage.vue
@@ -1,33 +1,13 @@
 <template>
   <div class="container mt-5">
     <div class="jumbotron">
-      <h1 class="display-title">{{ title }}</h1>
-      <img :src="image" alt="" />
+      <h1 class="title">{{ title }}</h1>
+      <img :src="image" class="img-fluid mx-auto d-block" />
       <hr class="my-4" />
       <p class="lead">{{ description }}</p>
     </div>
   </div>
 </template>
-
-<style scoped>
-img {
-  height: 300px;
-  display: block;
-  margin: 0 auto;
-}
-.display-title {
-  text-align: center;
-  text-emphasis-color: green;
-}
-.page {
-  text-align: center;
-  max-width: 800px;
-  margin: 0 auto;
-}
-p {
-  text-align: left;
-}
-</style>
 
 <script>
 export default {
@@ -48,3 +28,13 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.title {
+  text-align: center;
+  text-emphasis-color: green;
+}
+p {
+  text-align: left;
+}
+</style>
