f726a904cc76582ccf1bfbc9b3a491d8a4125417
StefanoAbatiello <80035914+StefanoAbatiello@users.noreply.github.com>
2022-06-27 15:51:02 +0200

    footer fixed

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="footer">
-    <div class="container row row-cols-2 my-2 p-0 justify-content-between">
-      <div class="col-lg-5 col-md-4 col-xs-3 my-3 mx-0">
+    <div class="container row row-cols-2 my-4 p-0 justify-content-between">
+      <div class="col-lg-5 col-md-6 col-sm-7 px-0">
         <a href="/">
           <img
             :src="require(`~/assets/logo.png`)"
-            class="img responsive logo-img"
+            class="img responsive logo-sito"
             alt="Logo"
             href="/"
           />
         </a>
       </div>
-      <div class="col-lg-5 col-md-4 col-xs-3 px-0 my-4 comune">
-        <a
-          href="https://www.comune.trieste.it/"
-          target="_blank"
-        >
+      <div class="col-lg-2 col-md-3 col-sm-4 px-0 comune">
+        <a href="https://www.comune.trieste.it/" target="_blank">
           <img
             :src="require(`~/assets/logo-trieste.png`)"
-            class="img trieste"
+            class="img logo-comune"
             alt="comune-logo"
-            style="height: 100px"
           />
         </a>
-        <p class="col-md-8 col-sm-1 info">Tel. 04 06754850</p>
+        <p class="info">Tel. 04 06754850</p>
       </div>
     </div>
     <hr class="divider" />
@@ -36,24 +32,6 @@
 <script>
 export default {
   name: 'TheFooter',
-  // data() {
-  //   return {
-  //     footerList: [
-  //       {
-  //         name: 'About',
-  //         path: '/about/',
-  //       },
-  //       {
-  //         name: 'Contacts',
-  //         path: '/contacts/',
-  //       },
-  //       {
-  //         name: 'Policy & Privacy',
-  //         path: '/policy/',
-  //       },
-  //     ],
-  //   }
-  // },
 }
 </script>
 
@@ -64,35 +42,34 @@ export default {
   position: absolute;
   bottom: 0px;
 }
-.logo-img {
-  width: 70%;
-  height: 5rem;
-}
-.text-box {
-  margin-left: 10%;
-  width: 80%;
-  text-align: right;
-  color: white;
-  /* border-top: 0.5px solid grey; */
-}
-.comune{
-  position: absolute;
-  right: 20px;
-}
 .container {
   height: 7rem;
+  width: 90%;
   margin-left: 50%;
   transform: translate(-50%, 0);
 }
+.logo-sito {
+  width: 70%;
+  height: 6rem;
+}
+.logo-comune {
+  height: 6rem;
+}
 .divider {
-  width: 80%;
+  width: 90%;
   color: white;
   margin-left: 50%;
   transform: translate(-50%, 0);
   margin-top: 45px;
   margin-bottom: 0px;
 }
-@media only screen and (max-width: 770px) {
+.text-box {
+  margin-left: 10%;
+  width: 80%;
+  text-align: right;
+  color: white;
+}
+/* @media only screen and (max-width: 770px) {
   .info {
     visibility: hidden;
   }
@@ -101,5 +78,5 @@ export default {
     width: 200px;
     margin-left: 0px;
   }
-}
+} */
 </style>
